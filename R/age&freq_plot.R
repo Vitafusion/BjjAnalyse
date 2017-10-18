93a4b8a641277b5a66431931e0189747e5740901
zhengzhi lin <linzhengzhizz@163.com>
2017-10-17 22:21:07 -0400

    make use of ggplot in one function: age&freq_plot

--- R/age&freq_plot.R
+++ R/age&freq_plot.R
@@ -1,12 +1,19 @@
 #' plot age&frequency data
-#' @param x an object
-#' @return k a plot
+#' @param x a dataframe
 #' @export
+#' @import ggplot2
+#' @import tidyverse
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
 #' @importFrom graphics  barplot
 #'
 
 
 age_freq <- function(x)
 {
-  barplot(table(x),ylim = c(0,60),main = "age&trainning frequency")
+  y <- x %>% select(age)
+  z <- table(y)
+  z <- as.data.frame(z)
+  g <- ggplot(z,aes(y,Freq))
+  g + geom_bar(stat = "identity") + ggtitle("Age Frequency") + xlab("Age") + ylab("Freq")
 }
