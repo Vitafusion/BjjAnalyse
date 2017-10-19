6606f74fc67133cce51bf55015dffb5e1d0ba0e9
zhengzhi lin <linzhengzhizz@163.com>
2017-10-18 21:10:33 -0400

    modify the vigneettes's last example's discription. By Zhengzhi Lin

--- R/self_rank.R
+++ R/self_rank.R
@@ -5,7 +5,7 @@
 #' @import tidyverse
 #' @importFrom dplyr select
 #' @importFrom magrittr %>%
-#'
+
 
 
 self_rank <- function(x)
@@ -14,7 +14,10 @@ self_rank <- function(x)
   t <- table(t)
   t <- as.data.frame(t)
   colnames(t) <- c("rank","freq")
-  g <- ggplot(t, aes(x="", y = freq, fill = rank)) + geom_bar(width = 1, stat = "identity")
-  pie <- g + coord_polar("y", start = 0) + ylab(" ") + scale_fill_manual(values = c("black","blue","brown","purple","white")) + xlab(" ")
+  myLabel <- as.vector(t$rank)
+  myLabel <- paste(myLabel, "(", round(t$freq/sum(t$freq) * 100, 2), "%)", sep = "")
+  g <- ggplot(t, aes(x="", y = freq, fill = rank)) + geom_bar(width = 1,stat = "identity")
+  g + coord_polar("y", start = 0) + xlab(" ") + ylab(" ") + scale_fill_manual(values = c("black","blue","brown","purple","white"),breaks = t$rank, labels = myLabel) +theme(axis.ticks = element_blank()) + theme(axis.text.x = element_blank())
+
 
 }
